700f37d714b5135f6c581073dc97451f4533e99c
Cloe <49467223+cloebot@users.noreply.github.com>
2019-07-10 15:01:00 -0700

    reusable code and some bug fix
    I accidentally found your code which will be very useful for my project. I would like to modify your "func_extract_clang.py" to make it a little bit more reusable. I will update more if there is more. Thanks!

--- func_extract_clang.py
+++ func_extract_clang.py
@@ -14,9 +14,10 @@ def write_to_csv(file_list, output_file):
     print "The output file is written to", output_file
 
 
-def get_ast(input_file, temp_file):
+def get_ast(source_folder, input_file, temp_file):
+    cmd = "clang -Xclang -ast-dump -I " + source_folder + " " + source_folder + input_file + " > " + temp_file
+    input_file_name = re.findall(r"([a-zA-Z0-9_]*.cpp|[a-zA-Z0-9_]*.h)", input_file)
 
-    cmd = "clang -Xclang -ast-dump -I /home/yuedeji/firmware/code_clone/bellon_c/postgresql/src/include/ " + input_file + " > " + temp_file
     os.system(cmd)
 
     ast_list = []
@@ -32,29 +33,30 @@ def get_ast(input_file, temp_file):
             continue
 #        print func_str
 #        m = re.findall(r"(\<.*>\s[a-zA-Z0-9_]*)", func_str)
-        m = re.findall(r"(\<.*\,\sline.*\>\s[a-zA-Z0-9_]*)", func_str)
+        m = re.findall(r"(\<.*\,\sline.*\>\sline:[0-9]*:[0-9]*\s[a-zA-Z0-9_]*)", func_str)
         if m:
             func_info = (re.sub('\'|\"', '', m[0])).split(' ')
 #            print func_str
 #            print func_info
+            # func_name = func_info[-1]
             func_name = func_info[-1]
             start_line = int((func_info[0]).split(':')[1])
             end_line = int((func_info[1]).split(':')[1])
 #            temp_line = [input_file, func_name, start_line, end_line, end_line - start_line + 1]
-            result_list.append([input_file, func_name, start_line, end_line, end_line - start_line + 1])
+            result_list.append([input_file_name[-1], func_name, start_line, end_line, end_line - start_line + 1])
 
     return result_list
 
 #~/firmware/code_clone/bellon_c/postgresql/src/backend/nodes/equalfuncs.c > clang.log
 
-def source_to_ast(input_file, output_file):
+def source_to_ast(source_folder, input_file, output_file):
 
-    func_list = get_ast(input_file, output_file)
+    func_list = get_ast(source_folder, input_file, output_file)
     write_to_csv(func_list, output_file)
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 3:
-        print "python func_extract_clang.py <input_file> <output_file>"
+    if len(sys.argv) != 4:
+        print "python func_extract_clang.py <source_folder> <input_file> <output_file>"
         exit(-1)
-    source_to_ast(sys.argv[1], sys.argv[2])
+    source_to_ast(sys.argv[1], sys.argv[2], sys.argv[3])
